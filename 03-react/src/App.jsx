076ed731e93b119d4daa1e20b708015d84b5336b
ElmaicDev <ramirezsanchezmaicolesneider@gmail.com>
2026-01-07 22:20:14 -0500

    Creando las rutas de forma no declarativa para eliminar el uso de condicionales. Se creó Route y se cambió en App

--- 03-react/src/App.jsx
+++ 03-react/src/App.jsx
@@ -4,25 +4,27 @@ import { HomePage } from "../pages/Home.jsx"
 import { SearchPage } from "../pages/Search.jsx"
 import { NotFoundPage } from "../pages/404.jsx"
 import { useRouter } from "../hooks/useRouter.jsx"
+import { Route } from "../components/Route.jsx"
 
 
 function App() {
      
-    const {currentPath} = useRouter()
+    // const {currentPath} = useRouter()
 
-    let page = <NotFoundPage/>
+    // let page = <NotFoundPage/>
 
-    if(currentPath === '/'){
-        page = <HomePage/>
-    }
-    else if(currentPath === '/search'){
-        page = <SearchPage/>
-    }
+    // if(currentPath === '/'){
+    //     page = <HomePage/>
+    // }
+    // else if(currentPath === '/search'){
+    //     page = <SearchPage/>
+    // }
 
     return (
     <>
         <Header></Header>
-            {page}
+            <Route path="/" component={HomePage} />
+            <Route path="/search" component={SearchPage} />
         <Footer></Footer>
     </>
   )
